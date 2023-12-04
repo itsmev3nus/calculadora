ebeadd997cc98ceb9ac9a898114ee1970412534e
Júlia Oliveira <141285162+itsmev3nus@users.noreply.github.com>
2023-12-04 20:08:33 -0300

    Calculadora

---
+++ Teste_V3nus/src/main/java/com/mycompany/teste_v3nus/Teste_V3nus.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.teste_v3nus;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class Teste_V3nus {
+
+    public static void main(String[] args) {
+        new Calculadora_da_Venus().setVisible(true);
+                
+                
+                
+                
+    
+    }
+}
